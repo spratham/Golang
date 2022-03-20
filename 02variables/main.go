e33810efaa8b0222dd0f11615eb593a4398ec11a
spratham <pratham919singh@gmail.com>
2022-03-20 13:49:01 +0530

    first commit

---
+++ 02variables/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x = 1.5
+	fmt.Println(x)
+	fmt.Printf("Variable type: %T \n", x)
+	fmt.Print()
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var integer string = "pratham"
+	fmt.Println(integer)
+	fmt.Printf("Variable type: %T \n", integer)
+}
