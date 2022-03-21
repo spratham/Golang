87c766cd49ef74907fb6650e6f9bf12ef861c1d7
spratham <pratham919singh@gmail.com>
2022-03-21 10:53:49 +0530

    3rd commit

--- 03slices/main.go
+++ 03slices/main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	var courses = []string{"reactjs", "js", "swift", "python", "ruby"}
+	fmt.Println(courses)
 
-	var integer string = "pratham"
-	fmt.Println(integer)
-	fmt.Printf("Variable type is: %T \n", integer)
-	fmt.Printf("Today mayank taught me github")
+	var index int = 2
+
+	courses = append(courses[:index], courses[index+1:]...)
+	fmt.Println(courses)
+
 }
