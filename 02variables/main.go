1d17eef347b4b95642e337e681bf8e51d9f7d39a
spratham <pratham919singh@gmail.com>
2022-03-21 10:21:38 +0530

    second commit

--- 02variables/main.go
+++ 02variables/main.go
@@ -6,6 +6,6 @@ func main() {
 
 	var x = 1.5
 	fmt.Println(x)
-	fmt.Printf("Variable type: %T \n", x)
+	fmt.Printf("Variable type is: %T \n", x)
 	fmt.Print()
 }
---
+++ 03slices/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var integer string = "pratham"
+	fmt.Println(integer)
+	fmt.Printf("Variable type is: %T \n", integer)
+	fmt.Printf("Today mayank taught me github")
+}
